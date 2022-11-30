4c24274270d14691d8022da6f4e249eb5f388943
yatzu <113869639+yasmina901017@users.noreply.github.com>
2022-11-30 23:06:33 +0800

    1130

---
+++ Assets/Scripts/FinalManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+namespace WineGiraffe
+{
+    /// <summary>
+    /// 結束管理器
+    /// </summary>
+
+    public class FinalManager : MonoBehaviour
+    {
+
+        public static FinalManager instance;
+        /// <summary>
+        /// 結束畫布
+        /// </summary>
+        private CanvasGroup groupFinal;
+        /// <summary>
+        /// 結束標題
+        /// </summary>
+        private TextMeshProUGUI textTitle;
+        /// <summary>
+        /// 重新遊戲
+        /// </summary>
+        private Button btnReplay;
+        private void Awake()
+        {
+            instance = this;
+
+            groupFinal = GameObject.Find("結束畫布").GetComponent<CanvasGroup>();
+            textTitle = GameObject.Find("結束標題").GetComponent<TextMeshProUGUI>();
+            btnReplay = GameObject.Find("重新遊戲").GetComponent<Button>();
+            btnReplay.onClick.AddListener(Replay);
+        }
+        /// <summary>
+        /// 遊戲結束
+        /// </summary>
+        /// <param name="title">遊戲標題</param>
+        public void GameOver(string title)
+        {
+            textTitle.text = title;
+
+            StartCoroutine(FadeIn());
+        }
+
+        /// <summary>
+        /// 淡入
+        /// </summary>
+        private IEnumerator FadeIn()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                groupFinal.alpha += 0.1f;
+                yield return new WaitForSeconds(0.02f);
+            }
+            groupFinal.interactable = true;
+            groupFinal.blocksRaycasts = true;
+
+        }
+
+        private void Replay()
+        {
+            SceneManager.LoadScene("開始選單");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerFinal.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace WineGiraffe
+
+{
+    /// <summary>
+    /// 玩家結束管理:死亡或過關
+    /// </summary>
+
+    public class PlayerFinal : MonoBehaviour
+    {
+
+        private void OnDestroy()
+        {
+            //死亡
+            FinalManager.instance.GameOver("挑戰失敗");
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            //過關
+            FinalManager.instance.GameOver("遊戲通過");
+        }
+    }
+}
