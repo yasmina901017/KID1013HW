0f0777d06e66d51b8d3617543b0ab69099f860fa
yatzu <113869639+yasmina901017@users.noreply.github.com>
2022-11-30 23:21:38 +0800

    1130

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace WineGiraffe
+{
+    /// <summary>
+    /// 選單管理器
+    /// </summary>
+
+    public class MenuManager : MonoBehaviour
+    {
+        /// <summary>
+        /// 開始遊戲
+        /// </summary>
+        private Button btnPlay;
+        private void Awake()
+        {
+            btnPlay = GameObject.Find("開始遊戲").GetComponent<Button>();
+            btnPlay.onClick.AddListener(StartGame);
+        }
+        private void StartGame()
+        {
+            SceneManager.LoadScene("遊戲場景");
+        }
+    }
+}
